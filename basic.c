9ed516612745aa62023f204de6962f08aceaedbf
shahadathhs <shahadathhossensajib732@gmail.com>
2025-09-29 16:35:40 +0600

    feat: c setup with vs code

---
+++ basic.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+    int number;
+
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    printf("You entered: %d\n", number);
+
+    return 0;
+}
