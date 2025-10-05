09dd9b1df906d958cf07d7f8fea89e98f25ed7e4
shahadathhs <shahadathhossensajib732@gmail.com>
2025-10-05 14:46:48 +0600

    feat: explore basic data type and variable

--- basic.c
+++ basic.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
 
-int main(void) {
+int main(void)
+{
     int number;
 
-    printf("Enter a number: ");
+    printf("Hello \n");
+    printf("World \n \n");
+
+    int num1;
+    num1 = 10;
+    printf("%d \n", num1);
+
+    float f = 1.22;
+    printf("%f \n", f);
+    printf("%.2f \n", f);
+
+    char c = '@';
+    printf("%c \n", c);
+
+    printf("\nEnter a number: ");
     scanf("%d", &number);
 
     printf("You entered: %d\n", number);
