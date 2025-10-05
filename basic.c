8f9510f1049a9900d3521d655ccd784fef6f79f7
shahadathhs <shahadathhossensajib732@gmail.com>
2025-10-05 15:32:36 +0600

    feat: long logn int & double

--- basic.c
+++ basic.c
@@ -3,25 +3,23 @@
 
 int main(void)
 {
-    printf("Hello \n");
-    printf("World \n \n");
+    printf("Hello\n");
+    printf("World\n\n");
 
-    int num1;
-    num1 = 10;
-    printf("%d \n", num1);
+    int num1 = 10;
+    printf("%d\n", num1);
 
     float f = 1.22;
-    printf("%f \n", f);
-    printf("%.2f \n", f);
+    printf("%f\n", f);
+    printf("%.2f\n", f);
 
     char c = '@';
-    printf("%c \n", c);
+    printf("%c\n", c);
 
     bool a = true;
     bool b = false;
-
-    printf("%d \n", a);
-    printf("%d \n", b);
+    printf("%d\n", a);
+    printf("%d\n", b);
 
     int number;
     printf("\nEnter a number: ");
@@ -38,10 +36,19 @@ int main(void)
     int ini;
     float inf;
     char inc;
-    printf("\nEnter int float & char: ");
+    printf("\nEnter int, float, and char: ");
     scanf("%d %f %c", &ini, &inf, &inc);
+    printf("\nYou entered: %d %.2f %c\n", ini, inf, inc);
 
-    printf("\nYour entered int, float & char: %d %f %c \n",&ini, inf, inc);
+    long long bigNum;
+    printf("\nEnter a long long integer: ");
+    scanf("%lld", &bigNum);
+    printf("\nYour entered long long: %lld\n", bigNum);
+
+    double dnum;
+    printf("\nEnter a double: ");
+    scanf("%lf", &dnum);
+    printf("\nYour entered double: %.5lf\n", dnum);
 
     return 0;
 }
