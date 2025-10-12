f0260fb4e6148110e5c0a2cf7bba19474962c0d9
shahadathhs <shahadathhossensajib732@gmail.com>
2025-10-12 20:50:28 +0600

    feat: even number problem fixed

---
+++ even_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(void)
+{
+    int N;
+
+    scanf("%d", &N);
+
+    for (int i = 0; i <= N; i++)
+    {
+        if(i % 2 == 0 && i > 0)
+        {
+            printf("%d\n", i);
+        }
+    }
+    
+
+    return 0;
+}
