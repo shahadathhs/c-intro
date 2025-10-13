19e585954383d8ac16de04b2771b3044b0715ce9
shahadathhs <shahadathhossensajib732@gmail.com>
2025-10-13 15:16:44 +0600

    feat: mulitplication table problem solved

---
+++ F_Multiplication_table.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(void)
+{
+    int input;
+
+    scanf("%d", &input);
+
+    for (int i = 1; i <= 12; i++)
+    {
+       printf("%d * %d = %d\n", input, i, input * i);
+    }
+    
+    return 0;
+}
