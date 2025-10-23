6b8938c3d5472bb5dad4238a84391163762eb471
shahadathhs <shahadathhossensajib732@gmail.com>
2025-10-23 19:44:54 +0600

    feat: string compare

---
+++ string_compare.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+int main(void)
+{
+    char a[101], b[101];
+    scanf("%s %s", a,b);
+
+    int i = 0;
+    while (true)
+    {
+        if (a[i] == '\0' && b[i] == '\0')
+        {
+            printf("Equal");
+            break;
+        }
+        else if (a[i] == '\0')
+        {
+            printf("A is Smaller");
+            break;
+        }
+        else if (b[i] == '\0')
+        {
+            printf("B is Smaller");
+            break;
+        }
+        else if (a[i] < b[i])
+        {
+            printf("A is Smaller");
+            break;
+        }
+        else if (a[i] > b[i])
+        {
+            printf("B is Smaller");
+            break;
+        }
+        else if (a[i] == b[i])
+        {
+            i++;
+        }
+
+    }
+
+    return 0;
+}
