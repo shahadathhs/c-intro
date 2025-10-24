3911cc48d696ca7bcfdae11a3498556ce06a9805
shahadathhs <shahadathhossensajib732@gmail.com>
2025-10-24 20:40:10 +0600

    feat: a3 problem 2

--- assignment-three.c
+++ assignment-three.c
@@ -1,32 +1,70 @@
+// #include<stdio.h>
+
+// int main(void)
+// {
+//     int n;
+//     scanf("%d", &n);
+
+//     int a[n];
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         scanf("%d", &a[i]);
+//     }
+
+//     int d2 = 0, d3 = 0;
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         if (a[i] % 2 == 0)
+//         {
+//             d2++;
+//         }
+//         else if (a[i] % 3 == 0)
+//         {
+//             d3++;
+//         }
+//     }
+
+//     printf("%d %d", d2, d3);
+    
+//     return 0;
+// }
 #include<stdio.h>
+#include<string.h>
 
 int main(void)
 {
-    int n;
-    scanf("%d", &n);
+    char s[100000];
+    scanf("%s", s);
 
-    int a[n];
+    int length = strlen(s);
 
-    for (int i = 0; i < n; i++)
+    char v[5] = {'a', 'e', 'i', 'o', 'u'};
+
+    int c = 0;
+
+    for (int i = 0; i < length; i++)
     {
-        scanf("%d", &a[i]);
+        int isVowel = 0;
+        for (int j = 0; j < 5; j++)
+        {
+            if(s[i] == v[j])
+            {
+                isVowel = 1;
+                break;
+            }
+        }
+
+        if (!isVowel)
+        {
+            c++;
+        }
+        
     }
 
-    int d2 = 0, d3 = 0;
-
-    for (int i = 0; i < n; i++)
-    {
-        if (a[i] % 2 == 0)
-        {
-            d2++;
-        }
-        else if (a[i] % 3 == 0)
-        {
-            d3++;
-        }
-    }
-
-    printf("%d %d", d2, d3);
+    printf("%d", c);
+    
     
     return 0;
 }
